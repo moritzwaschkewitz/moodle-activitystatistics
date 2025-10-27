8fe26bac19d642149830cb503a1df03cc4d84d97
Moritz Waschkewitz <moritz.waschkewitz@hs-niederrhein.de>
2025-10-27 14:07:41 +0100

    First showcase of database entries in a simple table

--- index.php
+++ index.php
@@ -26,5 +26,39 @@ $PAGE->set_title('Hello World - Title');
 $PAGE->set_heading('Hello World - Heading');
 
 echo $OUTPUT->header();
-echo html_writer::tag('h2', 'Hello World');
+
+global $DB;
+
+$sql = "
+    SELECT l.activityname,
+           c.count,
+           c.timestamp
+      FROM {tool_activitystatistics_lookup} l
+      JOIN {tool_activitystatistics_counts} c
+        ON l.id = c.activityid
+     WHERE c.timestamp = (
+         SELECT MAX(c2.timestamp)
+           FROM {tool_activitystatistics_counts} c2
+          WHERE c2.activityid = l.id
+     )
+  ORDER BY l.activityname ASC
+";
+
+$records = $DB->get_records_sql($sql);
+
+$table = new html_table();
+$table->head = ['Activity', 'Count', 'Last registered change'];
+$table->data = [];
+
+foreach ($records as $record) {
+    $table->data[] = [
+        format_string($record->activityname),
+        format_string($record->count),
+        userdate($record->timestamp)
+    ];
+}
+
+echo html_writer::tag('h2', 'Activity Statistics');
+echo html_writer::table($table);
+
 echo $OUTPUT->footer();
